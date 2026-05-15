f963a1969002368d19a40ec4dc66c451e6a86568
Darkings <darkings@foxmail.com>
2026-05-15 17:41:28 +0800

    消除 Obsidian CSS 审核警告

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,7 @@
 export default {
   extends: ["stylelint-config-standard-scss"],
   rules: {
+    "color-hex-length": "long",
     "declaration-no-important": true,
     "max-nesting-depth": 3,
     "selector-max-id": 0,
